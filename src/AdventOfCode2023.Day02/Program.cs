560072f2c04aa2af37e12781e12c10fca805a8f4
Bas Verweij <bas@basverweij.nl>
2023-12-09 23:12:14 +0100

    feat: complete day 2, puzzle 1

---
+++ src/AdventOfCode2023.Day02/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2023.Day02;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var games = lines.Select(Game.ParseLine);
+
+var puzzle1 = games.Where(IsPossible).Sum(g => g.Id);
+
+Console.WriteLine($"Day 1 - Puzzle 1: {puzzle1}");
+
+static bool IsPossible(
+    Game game)
+{
+    const int maxRed = 12;
+
+    const int maxGreen = 13;
+
+    const int maxBlue = 14;
+
+    return game is { MaxRed: <= maxRed, MaxGreen: <= maxGreen, MaxBlue: <= maxBlue };
+}

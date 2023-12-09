d0838f26356ce3b503fdec1dd9e006b179f6bce3
Bas Verweij <bas@basverweij.nl>
2023-12-09 23:17:36 +0100

    feat: complete day 2, puzzle 2

--- src/AdventOfCode2023.Day02/Program.cs
+++ src/AdventOfCode2023.Day02/Program.cs
@@ -6,7 +6,11 @@ var games = lines.Select(Game.ParseLine);
 
 var puzzle1 = games.Where(IsPossible).Sum(g => g.Id);
 
-Console.WriteLine($"Day 1 - Puzzle 1: {puzzle1}");
+Console.WriteLine($"Day 2 - Puzzle 1: {puzzle1}");
+
+var puzzle2 = games.Select(Power).Sum();
+
+Console.WriteLine($"Day 2 - Puzzle 2: {puzzle2}");
 
 static bool IsPossible(
     Game game)
@@ -18,4 +22,10 @@ static bool IsPossible(
     const int maxBlue = 14;
 
     return game is { MaxRed: <= maxRed, MaxGreen: <= maxGreen, MaxBlue: <= maxBlue };
+}
+
+static int Power(
+    Game game)
+{
+    return game.MaxRed * game.MaxGreen * game.MaxBlue;
 }

d6aa9377c560f7b8e3c0811e74ebd10c1fdbc987
basverweij <basverweij@users.noreply.github.com>
2023-12-17 16:47:11 +0100

    Day 5 (#5)

---
+++ src/AdventOfCode2023.Day05/Mapping.cs
@@ -0,0 +1,125 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+internal sealed record Mapping(
+    string From,
+    string To)
+{
+    internal SortedSet<MappingRange> Ranges { get; } = [];
+
+    internal long Map(
+        long sourceValue)
+    {
+        foreach (var range in Ranges)
+        {
+            if (range.TryMap(sourceValue, out var destinationValue))
+            {
+                return destinationValue;
+            }
+        }
+
+        return sourceValue;
+    }
+
+    internal IEnumerable<Range> MapRange(
+        Range sourceRange)
+    {
+        foreach (var range in Ranges)
+        {
+            if (sourceRange.Start >= range.SourceRangeEndExclusive)
+            {
+                continue;
+            }
+
+            if (sourceRange.EndExclusive <= range.SourceRangeStart)
+            {
+                // source range is completely unmapped
+
+                yield return sourceRange;
+
+                yield break;
+            }
+
+            if (sourceRange.Start < range.SourceRangeStart)
+            {
+                // source range is partially unmapped
+
+                var unmappedLength = range.SourceRangeStart - sourceRange.Start;
+
+                yield return new(sourceRange.Start, unmappedLength);
+
+                sourceRange = new(range.SourceRangeStart, sourceRange.Length - unmappedLength);
+            }
+
+            if (sourceRange.EndExclusive <= range.SourceRangeEndExclusive)
+            {
+                // source range is completely mapped
+
+                yield return new(sourceRange.Start - range.SourceRangeStart + range.DestinationRangeStart, sourceRange.Length);
+
+                yield break;
+            }
+
+            // source range is partially mapped
+
+            var mappedLength = range.SourceRangeEndExclusive - sourceRange.Start;
+
+            yield return new(sourceRange.Start - range.SourceRangeStart + range.DestinationRangeStart, mappedLength);
+
+            sourceRange = new(sourceRange.Start + mappedLength, sourceRange.Length - mappedLength);
+        }
+
+        yield return sourceRange;
+    }
+}
+
+internal sealed record MappingRange(
+    long DestinationRangeStart,
+    long SourceRangeStart,
+    long RangeLength
+) :
+    IComparable<MappingRange>
+{
+    internal long SourceRangeEndExclusive => SourceRangeStart + RangeLength;
+
+    /// <summary>
+    /// Sort by SourceRangeStart.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public int CompareTo(
+        MappingRange? other)
+    {
+        if (other is null ||
+            SourceRangeStart == other.SourceRangeStart)
+        {
+            return 0;
+        }
+
+        return SourceRangeStart < other.SourceRangeStart ? -1 : 1;
+    }
+
+    internal bool TryMap(
+        long sourceValue,
+        out long destinationValue)
+    {
+        if (sourceValue < SourceRangeStart ||
+            sourceValue >= SourceRangeStart + RangeLength)
+        {
+            destinationValue = 0;
+
+            return false;
+        }
+
+        destinationValue = sourceValue - SourceRangeStart + DestinationRangeStart;
+
+        return true;
+    }
+};
+
+internal sealed record Range(
+    long Start,
+    long Length)
+{
+    internal long EndExclusive => Start + Length;
+}
---
+++ src/AdventOfCode2023.Day05/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Immutable;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var seeds = ParseNumbers(lines[0][7..]);
+
+var mappings = ParseMappings(lines[2..]);
+
+var locations = seeds.Select(s => MapSeed(mappings, s));
+
+var puzzle1 = locations.Min();
+
+Console.WriteLine($"Day 5 - Puzzle 1: {puzzle1}");
+
+var seedRanges = seeds.Chunk(2).Select(c => new Range(c[0], c[1])).ToImmutableArray();
+
+var locationRanges = seedRanges.SelectMany(r => MapSeedRange(mappings, r));
+
+var puzzle2 = locationRanges.Min(r => r.Start);
+
+Console.WriteLine($"Day 5 - Puzzle 2: {puzzle2}");
+
+static ImmutableArray<long> ParseNumbers(
+    string value) =>
+    value
+        .Split(' ')
+        .Select(long.Parse)
+        .ToImmutableArray();
+
+static Dictionary<string, Mapping> ParseMappings(
+    string[] lines)
+{
+    var mappings = new Dictionary<string, Mapping>();
+
+    Mapping? mapping = null;
+
+    foreach (var line in lines)
+    {
+        if (mapping is null)
+        {
+            var parts = line[..^5].Split('-');
+
+            mapping = new(parts[0], parts[2]);
+
+            mappings[mapping.From] = mapping;
+
+            continue;
+        }
+
+        if (line.Length == 0)
+        {
+            mapping = null;
+
+            continue;
+        }
+
+        var range = ParseNumbers(line);
+
+        mapping.Ranges.Add(new(range[0], range[1], range[2]));
+    }
+
+    return mappings;
+}
+
+static long MapSeed(
+    Dictionary<string, Mapping> mappings,
+    long value)
+{
+    var mapping = mappings["seed"];
+
+    do
+    {
+        value = mapping.Map(value);
+    }
+    while (mappings.TryGetValue(mapping.To, out mapping));
+
+    return value;
+}
+
+static IEnumerable<Range> MapSeedRange(
+    Dictionary<string, Mapping> mappings,
+    Range range)
+{
+    var ranges = ImmutableArray.Create(range);
+
+    var mapping = mappings["seed"];
+
+    do
+    {
+        ranges = ranges.SelectMany(r => mapping.MapRange(r)).ToImmutableArray();
+    }
+    while (mappings.TryGetValue(mapping.To, out mapping));
+
+    return ranges;
+}

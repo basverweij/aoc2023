08c8ad2ce54489185690aaeda710b11e39f4843b
Bas Verweij <bas@basverweij.nl>
2023-12-23 11:44:28 +0100

    feat: complete day 6, puzzle 2

--- src/AdventOfCode2023.Day06/GameUtil.cs
+++ src/AdventOfCode2023.Day06/GameUtil.cs
@@ -2,20 +2,20 @@ namespace AdventOfCode2023.Day06;
 
 internal static class GameUtil
 {
-    internal static int WinOptions(
-        int time,
-        int distance)
+    internal static long WinOptions(
+        long time,
+        long distance)
     {
         var d = Math.Sqrt(time * time - 4 * distance);
 
-        var min = (int)Math.Ceiling((time - d) / 2d);
+        var min = (long)Math.Ceiling((time - d) / 2d);
 
         if (min * time - min * min <= distance)
         {
             min++;
         }
 
-        var max = (int)Math.Floor((time + d) / 2d);
+        var max = (long)Math.Floor((time + d) / 2d);
 
         if (max * time - max * max <= distance)
         {
--- src/AdventOfCode2023.Day06/Program.cs
+++ src/AdventOfCode2023.Day06/Program.cs
@@ -9,13 +9,28 @@ var distances = ParseValues(lines[1]);
 
 var winOptions = times.Zip(distances).Select(z => GameUtil.WinOptions(z.First, z.Second)).ToImmutableArray();
 
-var puzzle1 = winOptions.Aggregate(1, (a, s) => a * s);
+var puzzle1 = winOptions.Aggregate(1L, (a, s) => a * s);
 
 Console.WriteLine($"Day 6 - Puzzle 1: {puzzle1}");
 
+var time = ParseValue(lines[0]);
+
+var distance = ParseValue(lines[1]);
+
+var puzzle2 = GameUtil.WinOptions(time, distance);
+
+Console.WriteLine($"Day 6 - Puzzle 2: {puzzle2}");
+
 static ImmutableArray<int> ParseValues(
-    string line) => line
-    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-    .Skip(1)
-    .Select(int.Parse)
-    .ToImmutableArray();
+    string line) =>
+    line
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Skip(1)
+        .Select(int.Parse)
+        .ToImmutableArray();
+
+static long ParseValue(
+    string line) =>
+    long.Parse(line
+        .Replace(" ", "")
+        .Split(':')[1]);

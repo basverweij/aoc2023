7beaf64a90ca6fc72becbdcf3240b4e89d8a0257
Bas Verweij <bas@basverweij.nl>
2023-12-23 14:31:29 +0100

    feat: complete day 8, puzzle 1

---
+++ src/AdventOfCode2023.Day08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Frozen;
+using AdventOfCode2023.Day08;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var instructions = lines[0].ToCharArray();
+
+var nodes = lines[2..].Select(Node.Parse).ToFrozenDictionary(n => n.Name);
+
+var (instructionPointer, steps, node) = (0, 0, nodes["AAA"]);
+
+for (; node.Name != "ZZZ"; steps++)
+{
+    node = instructions[instructionPointer] == 'L' ? nodes[node.Left] : nodes[node.Right];
+
+    instructionPointer = (instructionPointer + 1) % instructions.Length;
+}
+
+Console.WriteLine($"Day 8 - Puzzle 1: {steps}");
